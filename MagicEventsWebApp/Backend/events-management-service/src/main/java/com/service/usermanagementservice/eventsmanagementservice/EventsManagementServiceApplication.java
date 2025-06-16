f767b14a6812d3029d7b242d9eefc539a8012a4b
Daniela Magrì <82176325+Danix002@users.noreply.github.com>
2025-06-16 10:58:00 +0200

    feat: add template for events-management-service

---
+++ MagicEventsWebApp/Backend/events-management-service/src/main/java/com/service/usermanagementservice/eventsmanagementservice/EventsManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.usermanagementservice.eventsmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsManagementServiceApplication.class, args);
+    }
+
+}
---
+++ MagicEventsWebApp/Backend/events-management-service/src/test/java/com/service/usermanagementservice/eventsmanagementservice/EventsManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.usermanagementservice.eventsmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
