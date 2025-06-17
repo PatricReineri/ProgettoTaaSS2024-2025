39232a778cbb6125ec684a5e6cd3e73f83ad07ac
Daniela Magrì <82176325+Danix002@users.noreply.github.com>
2025-06-17 20:03:26 +0200

    chore: remove obsolete service

--- MagicEventsWebApp/Backend/guest-game-service/src/main/java/com/service/guestgameservice/repository/GameRepository.java
+++
@@ -1,7 +0,0 @@
-package com.service.guestgameservice.repository;
-import com.service.guestgameservice.model.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
