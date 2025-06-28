f7e1c5a46ea60600727518bc3b5e1288961893c2
Daniela Magrì <82176325+Danix002@users.noreply.github.com>
2025-06-28 08:47:35 +0200

    fix: add cors config in services

--- MagicEventsWebApp/Backend/gallery-service/src/main/java/com/service/galleryservice/config/CorsConfig.java
+++ MagicEventsWebApp/Backend/gallery-service/src/main/java/com/service/galleryservice/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.service.eventsmanagementservice.config;
+package com.service.galleryservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
