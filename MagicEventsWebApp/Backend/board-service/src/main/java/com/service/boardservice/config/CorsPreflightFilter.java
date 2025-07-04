4143e2193b7e45caba5e77c6bdf2f0abd4a0938e
Daniela Magrì <82176325+Danix002@users.noreply.github.com>
2025-07-04 16:05:52 +0200

    feat: add cors filter

---
+++ MagicEventsWebApp/Backend/board-service/src/main/java/com/service/boardservice/config/CorsPreflightFilter.java
@@ -0,0 +1,30 @@
+package com.service.boardservice.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class CorsPreflightFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_NO_CONTENT); // 204
+            response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+            response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "*");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+}
