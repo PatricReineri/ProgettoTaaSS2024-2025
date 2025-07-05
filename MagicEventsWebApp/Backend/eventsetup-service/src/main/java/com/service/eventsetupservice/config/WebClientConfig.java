6bc8377f507fa27cc19fd362e8d934f9b09a7270
PatricReineri <onepat96@gmail.com>
2025-07-05 10:36:46 +0200

    Edit WEBCLIENT SSL config

--- MagicEventsWebApp/Backend/eventsetup-service/src/main/java/com/service/eventsetupservice/config/WebClientConfig.java
+++ MagicEventsWebApp/Backend/eventsetup-service/src/main/java/com/service/eventsetupservice/config/WebClientConfig.java
@@ -1,9 +1,16 @@
 package com.service.eventsetupservice.config;
 
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import reactor.netty.http.client.HttpClient;
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import javax.net.ssl.SSLException;
 
 @Configuration
 public class WebClientConfig {
@@ -19,23 +26,51 @@ public class WebClientConfig {
     @Value("${services.guestgameservice.url}")
     private String guestGameServiceUrl;
 
+    private HttpClient createHttpClient() {
+        return HttpClient.create()
+                .secure(spec -> {
+                    try {
+                        spec.sslContext(SslContextBuilder
+                                .forClient()
+                                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                                .build()
+                        );
+                    } catch (SSLException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .responseTimeout(Duration.ofSeconds(30));
+    }
+
     @Bean
     public WebClient eventManagementWebClient(WebClient.Builder builder) {
-        return builder.baseUrl(eventManagementServiceUrl).build();
+        return builder
+                .baseUrl(eventManagementServiceUrl)
+                .clientConnector(new ReactorClientHttpConnector(createHttpClient()))
+                .build();
     }
 
     @Bean
     public WebClient galleryServiceWebClient(WebClient.Builder builder) {
-        return builder.baseUrl(galleryServiceUrl).build();
+        return builder
+                .baseUrl(galleryServiceUrl)
+                .clientConnector(new ReactorClientHttpConnector(createHttpClient()))
+                .build();
     }
 
     @Bean
     public WebClient boardServiceWebClient(WebClient.Builder builder) {
-        return builder.baseUrl(boardServiceUrl).build();
+        return builder
+                .baseUrl(boardServiceUrl)
+                .clientConnector(new ReactorClientHttpConnector(createHttpClient()))
+                .build();
     }
 
     @Bean
     public WebClient guestGameServiceWebClient(WebClient.Builder builder) {
-        return builder.baseUrl(guestGameServiceUrl).build();
+        return builder
+                .baseUrl(guestGameServiceUrl)
+                .clientConnector(new ReactorClientHttpConnector(createHttpClient()))
+                .build();
     }
 }
